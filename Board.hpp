7f3847d384d7cf4f21359bcdff0c11c61190c1ee
Marek Rychlik <rychlik@gmail.com>
2021-05-19 13:40:54 -0700

    *** empty log message ***

--- Board.hpp
+++ Board.hpp
@@ -1,33 +1,49 @@
+/**
+ * @file   Board.hpp
+ * @author Marek Rychlik <marek@cannonball.lan>
+ * @date   Wed May 19 13:40:37 2021
+ * 
+ * @brief  Othello game board
+ * 
+ * 
+ */
+
+#ifndef BOARD_HPP
+#define BOARD_HPP 1
+#else
+
 #include <string>
 #include <deque>
 
 class Board {
-  private:
-    char filled[8];
-    char coloredWhite[8];
-    char turnAndTile; //first bit is isWhite'sTurn, last 7 are number of tiles on board
-    signed char scoreInt;
+private:
+  char filled[8];
+  char coloredWhite[8];
+  char turnAndTile; //first bit is isWhite'sTurn, last 7 are number of tiles on board
+  signed char scoreInt;
 
-    bool isFilled(int, int); 
-    bool isWhite(int, int); 
-    void flipToWhite(int, int); 
-    void flipToBlack(int, int); 
+  bool isFilled(int, int); 
+  bool isWhite(int, int); 
+  void flipToWhite(int, int); 
+  void flipToBlack(int, int); 
   
-  public:
-    Board();
-    Board(Board*);
-    int score();
-    int value();
-    Board* move(bool,int,int);
-    int tileNum();
-    void copy(Board*);
-    std::string toStringSmall();
-    std::string toStringBig();
-    std::string toString();
-    std::string toString(bool);
-    std::deque<Board*> children(bool);
-    bool whitesTurn();
-    bool isLegal(bool, int, int);
-    bool anyLegalMoves(bool);
-    bool operator==(const Board&);
+public:
+  Board();
+  Board(Board*);
+  int score();
+  int value();
+  Board* move(bool,int,int);
+  int tileNum();
+  void copy(Board*);
+  std::string toStringSmall();
+  std::string toStringBig();
+  std::string toString();
+  std::string toString(bool);
+  std::deque<Board*> children(bool);
+  bool whitesTurn();
+  bool isLegal(bool, int, int);
+  bool anyLegalMoves(bool);
+  bool operator==(const Board&);
 };
+
+#endif // BOARD_HPP
