1db82912565ddce2b6c86a9a01365e0e61f73b0a
Marek Rychlik <rychlik@gmail.com>
2021-05-19 14:04:00 -0700

    *** empty log message ***

--- Board.hpp
+++ Board.hpp
@@ -23,17 +23,23 @@ public:
   Board* move(bool,int,int);
   int tileNum();
   void copy(Board*);
-  std::string toStringSmall();
-  std::string toStringBig();
-  std::string toString();
-  std::string toString(bool);
+
   std::deque<Board*> children(bool);
   bool whitesTurn();
   bool isLegal(bool, int, int);
   bool anyLegalMoves(bool);
   bool operator==(const Board&);
 
+  operator std::string() { return this->toString(); };
+
 private:
+
+  std::string toStringSmall();
+  std::string toStringBig();
+  std::string toString();
+  std::string toString(bool);
+
+
   char filled[8];
   char coloredWhite[8];
   char turnAndTile; //first bit is isWhite'sTurn, last 7 are number of tiles on board
