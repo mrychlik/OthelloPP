16eaf20c2dcf23f0a55dbb57bec2938e1faba815
Marek Rychlik <rychlik@gmail.com>
2021-05-19 20:51:00 -0700

    *** empty log message ***

--- Board.cpp
+++ Board.cpp
@@ -97,15 +97,15 @@ Board::move_bag_type Board::moves(bool playWhite) const
   move_bag_type move_bag;
   for( auto x = 0; x < 8; ++x) {
     for( auto y = 0; y < 8; ++y) {
-      if (isFilled(x,y)) continue;
+      if( isFilled(x,y) ) continue;
       int tmpx, tmpy, distance, end;
       int toFlip[8];
       for (int ray = 0; ray < 8; ray++) { //iter over cardinal + diagonals
 	distance = 1;
 	end = 0;
 	while (end == 0) {
-	  tmpx = x + (distance*order[ray][0]);
-	  tmpy = y + (distance*order[ray][1]);
+	  tmpx = x + distance * order[ray][0];
+	  tmpy = y + distance * order[ray][1];
 
 	  if (tmpx < 0 || tmpx > 7 || tmpy < 0 || tmpy > 7) end = 1; //ran off edge
 	  else if (!isFilled(tmpx,tmpy)) end = 2; //ran into an empty space
