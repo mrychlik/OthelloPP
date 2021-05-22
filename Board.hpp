e7437be975e55a2dafb99ac76ccfc854d956d733
Marek Rychlik <rychlik@gmail.com>
2021-05-22 01:31:18 -0700

    *** empty log message ***

--- Board.hpp
+++ Board.hpp
@@ -89,7 +89,7 @@ private:
   void setBlack(uint8_t x, uint8_t y); 
   void setColor(uint8_t x, uint8_t y); 
 
-  void flipDistance(uint8_t x, uint8_t y, uint8_t toFlip[8]) const;
+  void findFlipRadius(uint8_t x, uint8_t y, uint8_t flipRadius[8]) const;
 };
 
 
