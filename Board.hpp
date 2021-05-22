37dc3ab7266858df995f84e4ea423ce699d864c2
Marek Rychlik <rychlik@gmail.com>
2021-05-21 22:01:14 -0700

    *** empty log message ***

--- Board.hpp
+++ Board.hpp
@@ -54,7 +54,7 @@ public:
   int numTiles() const;
   int numWhiteTiles () const;
   int numBlackTiles () const;  
-  move_bag_type moves() const throw(std::bad_alloc);
+  move_bag_type moves() const;
   int score() const;
   bool isWhitesTurn() const;
   bool hasLegalMove() const;
