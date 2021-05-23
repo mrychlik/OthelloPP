749db83c64fa70cca127739d1ca0e4e608772c65
Marek Rychlik <rychlik@gmail.com>
2021-05-22 20:30:46 -0700

    *** empty log message ***

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -100,7 +100,7 @@ BOOST_AUTO_TEST_CASE(tree_node_size)
 BOOST_AUTO_TEST_CASE(tree_minmax)
 {
   TreeNode root;
-  int8_t depth = 14;
+  int depth = 14;
   root.minmax(Board::WHITE, depth);
   std::cout << "\nDepth : " << depth << "\n"
 	    << root
