a85f013b8471e058751e9260e5ed85f3a77d9741
Marek Rychlik <rychlik@gmail.com>
2021-05-22 21:42:10 -0700

    *** empty log message ***

--- GameTree.cpp
+++ GameTree.cpp
@@ -115,7 +115,7 @@ std::ostream& operator<<(std::ostream& s, const TreeNode& tree)
     << "\nValue: " << static_cast<int>(tree.value())
     << "\nMinMax value: " << static_cast<int>(tree.minmaxValue)
     << "\nMinMax depth: " << static_cast<int>(tree.minmaxDepth)
-    << "\nLast piece (x, y): << " << tree.x << ", " << tree.y;
+    << "\nLast piece (x, y): << " << tree.x << ", " << tree.y
     << std::endl;
   return s;
 }
