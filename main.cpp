0f8110da1f91e91e1097f93025be7352c8a83b23
Marek Rychlik <rychlik@gmail.com>
2021-05-22 20:51:23 -0700

    *** empty log message ***

--- main.cpp
+++ main.cpp
@@ -63,61 +63,21 @@ getPlayerMove(std::istream& s, const Board::move_bag_type& move_bag)
   throw std::runtime_error("Invalid move");
 }
 
+const int MAX_DEPTH =  7;
 
 /** 
  * @return 
  */
 int main() {
-  TreeNode absTreeRoot;
-  Player player = Board::WHITE;
+  Board::Player player = Board::WHITE;
+  TreeNode absTreeRoot(player);
+  TreeNode& currentNode(absTreeRoot);
 
   std::cout << absTreeRoot << std::endl;
 
-  while(!absTreeRoot.isLeaf()) {
-    absTreeRoot.minmax(player);
-
-    //get player move, if there is one
-    auto possiblePlayerMove = getPlayerMove(std::cin, absTreeRoot.moves());
-
-    //update tree values
-    absTreeRoot.updateTreeDesireablility(curTileNum); //pass numTiles as update#, no repeats
-    
-    //check for early game end
-    auto board = std::get<2>(possiblePlayerMove);
-    auto moves = board.moves();
-
-    if ( moves.empty() ) {
-      //board.switchPlayer();
-      auto moves = board.moves();
-      if( moves.empty()) {
-	std::cout << "No possible moves for either player, ending game" << std::endl;
-	if (board.score() > 0) {
-	  std::cout << "White Wins";
-	} else if(board.score() < 0) {
-	  std::cout << "Black Wins";
-	} else {
-	  std::cout << "Tie";
-	}
-	std::cout << std::endl;
-	return 0;
-      }
-    }
-  
-    //get best move + do so
-    auto bestResponse = absTreeRoot.bestMove(possiblePlayerMove);
-
-    //print board state
-    std::cout << *bestResponse << std::endl;
-
-    absTreeRoot = *bestResponse;
+  if(!currentNode.isLeaf()) {
+    currentNode.minmax(player, MAX_DEPTH);
   }
-  
-  //if (treeGenerator.joinable()) treeGenerator.join(); //hopefully this catches it finishing between this and previous line
-  std::cout << "No possible moves for either player, ending game" << std::endl;
-  if (absTreeRoot.score() > 0) std::cout << "White Wins";
-  else if (absTreeRoot.score() < 0) std::cout << "Black Wins";
-  else std::cout << "Tie";
-  std::cout << std::endl;
   return 0;
 
 }
