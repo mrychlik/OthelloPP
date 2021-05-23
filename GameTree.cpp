21b1424b6ee5d71402b10d2dff212282e9d74e38
Marek Rychlik <rychlik@gmail.com>
2021-05-23 14:27:04 -0700

    *** empty log message ***

--- GameTree.cpp
+++ GameTree.cpp
@@ -206,3 +206,47 @@ const TreeNode::children_type& TreeNode::children() const
   }
   return children_;
 }
+
+
+/** 
+ * Reads player move from a stream.
+ * Validates the move against moves.
+ * 
+ * @param s 
+ * @param move_bag
+ * 
+ * @return Triple (x, y, Board)
+ */
+Board::move_type
+GameTree::getHumanMove(std::istream& s)
+{
+  int x,y;
+
+  while(std::cin) {
+    std::cout << "Player Move x and y" << std::endl;
+    std::cin >> x >> y ; 
+    if(std::cin.fail()) {
+      std::cerr << "Bad x\n"; 
+      continue;
+    }
+  };
+
+  if(std::cin.bad()) {
+    throw std::runtime_error("Input stream bad during input of x and y.");
+  }
+    
+  if (x < 0 || x > 7 || y < 0 || y > 7) {
+    std::cerr << "Input value x or y is invalid: " << x << ", " << y << "\n";
+    throw std::runtime_error("Invalid input");
+  }
+
+  for( auto& m : move_bag) {
+    auto [x1, y1, board1] = m;
+    if( x1 == x && y1 == y) {
+      return m;
+    }
+  }
+
+  std::cerr << "Move is invalid: " << x << ", " << y << "\n";
+  throw std::runtime_error("Invalid move");
+}
