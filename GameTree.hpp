84df1a18fcafed96c041aa83ac62e70af7e70eb1
Marek Rychlik <rychlik@gmail.com>
2021-05-23 20:41:09 -0700

    *** empty log message ***

--- GameTree.hpp
+++ GameTree.hpp
@@ -53,8 +53,8 @@ public:
   
   bool isLeaf() const;
   const children_type& children() const;
-  Board::move_type getHumanMove(std::istream& s) const;
-  Board::move_type getComputerMove(int depth) const;
+  TreeNode& getHumanMove(std::istream& s) const;
+  TreeNode& getComputerMove(int depth) const;
   int nodeCount(int depth) const;
 
   int minmax(int8_t depth, value_type alpha = MIN_VAL, value_type beta = MAX_VAL) const;
