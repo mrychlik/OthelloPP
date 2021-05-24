a6b4e0f03e61a10aadf1f9ff089de863e0ce43f6
Marek Rychlik <rychlik@gmail.com>
2021-05-23 22:06:43 -0700

    *** empty log message ***

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 #include "Board.hpp"
 
 
-const   bool humanPlaysWhite = true; /**< Is human playing white? */
+const   bool humanPlaysWhite = false; /**< Is human playing white? */
 const   bool humanPlaysBlack = false; /**< Is human playing black? */
 
 const int MAX_DEPTH =  5;	/**< Depth to which examine the tree to compute the best move */
