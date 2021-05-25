05e5e317f220e78865c7863e904a82cc00b55010
Marek Rychlik <rychlik@gmail.com>
2021-05-25 16:01:23 -0700

    *** empty log message ***

--- MainLoop.hpp
+++ MainLoop.hpp
@@ -11,12 +11,14 @@
 #ifndef MAIN_LOOP_H
 #define MAIN_LOOP_H
 
+#include <iosfwd>
+
 extern int  max_depth[2];
 extern bool humanPlayer[2];
 extern int num_games;
 extern int computer_delay;
 
-extern int main_loop();
+extern int main_loop(std::istream& ins, std::ostream& os)
 
 
 #endif /* MAIN_LOOP */
