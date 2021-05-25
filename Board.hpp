bb156efa6b48094c51b90565ea970468ba4f0f08
Marek Rychlik <rychlik@gmail.com>
2021-05-24 21:15:27 -0700

    *** empty log message ***

--- Board.hpp
+++ Board.hpp
@@ -84,6 +84,9 @@ public:
 public:
   friend std::ostream& operator<<(std::ostream& s, const Board& b);  
   std::ostream& print(std::ostream& s, bool big = false) const;
+  
+protected:
+  int (Board::*staticEvaluator)() const;
 
 private:
 
