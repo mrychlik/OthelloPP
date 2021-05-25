9699d1ca726039fcdc7678c5933e7b9609b40299
Marek Rychlik <rychlik@gmail.com>
2021-05-24 18:16:57 -0700

    *** empty log message ***

---
+++ MainLoop.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file   MainLoop.hpp
+ * @author Marek Rychlik <marek@cannonball.lan>
+ * @date   Mon May 24 18:14:26 2021
+ * 
+ * @brief  Declarations for main loop implementation
+ * 
+ * 
+ */
+
+#ifndef MAIN_LOOP_H
+#define MAIN_LOOP_H
+
+int  max_depth[2];
+bool humanPlayer[2];
+int num_games;
+int computer_delay;
+
+int main_loop();
+
+
+#endif /* MAIN_LOOP */
