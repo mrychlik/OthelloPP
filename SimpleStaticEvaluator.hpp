245ccac00951c42f690fc6c39284dcba777e1930
Marek Rychlik <rychlik@gmail.com>
2021-05-25 16:38:44 -0700

    Modified standard flag

---
+++ SimpleStaticEvaluator.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file   SimpleStaticEvaluator.hpp
+ * @author Marek Rychlik <marek@cannonball.lan>
+ * @date   Mon May 24 23:21:49 2021
+ * 
+ * @brief  A trivial static evaluator
+ * 
+ * 
+ */
+
+#ifndef SIMPLE_STATIC_EVALUATOR_HPP
+#define SIMPLE_STATIC_EVALUATOR_HPP
+
+/**
+ * Uses score as value of the board. 
+ * 
+ */
+struct SimpleStaticEvaluator : public StaticEvaluator, public StaticEvaluatorTraits
+{
+  value_type operator()(const Board& b, Board::Player player, int depth) const
+  {
+    return b.score();
+  }
+};
+
+#endif 
