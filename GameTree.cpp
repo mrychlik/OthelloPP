8af04cdeb7b8437e816ba934095f1e30ff1466c1
Marek Rychlik <rychlik@gmail.com>
2021-05-24 17:37:22 -0700

    *** empty log message ***

--- GameTree.cpp
+++ GameTree.cpp
@@ -142,6 +142,7 @@ std::ostream& operator<<(std::ostream& s, const TreeNode& tree)
 int TreeNode::minmax(int8_t depth, value_type alpha, value_type beta) const
 {
   assert(depth >= 1);
+
   if(isLeaf() ) {
     minmaxValue = value();
     minmaxDepth = 0;
