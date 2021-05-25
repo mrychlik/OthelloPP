2116af3c043ad00c27013a934d057adec11481c7
Marek Rychlik <rychlik@gmail.com>
2021-05-25 01:06:23 -0700

    *** empty log message ***

--- GameTree.cpp
+++ GameTree.cpp
@@ -313,7 +313,7 @@ TreeNode& TreeNode::getComputerMove(const StaticEvaluator& evaluator, int depth)
 
     auto bestVal = minmax(evaluator, depth);
     for(auto child : children()) {
-      if(child->minmax(evaluator, depth-1) == bestVal) {
+      if(child->minmaxVal == bestVal) {
 	bestChildren.push_back(child);
       }
     }
