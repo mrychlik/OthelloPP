a5fd0727898427bfca71d819f13e95b592f13fce
Marek Rychlik <rychlik@gmail.com>
2021-05-24 23:02:52 -0700

    *** empty log message ***

---
+++ AbstractStaticEvaluator.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file   AbstractStaticEvaluator.hpp
+ * @author Marek Rychlik <marek@cannonball.lan>
+ * @date   Mon May 24 23:00:24 2021
+ * 
+ * @brief  Defines abstract base class of all evaluators
+ * 
+ * 
+ */
+
+#ifndef ABSTRACT_STATIC_EVALUATOR_HPP
+#define ABSTRACT_STATIC_EVALUATOR_HPP
+
+/**
+ * Virtual base class of all static evaluators
+ * 
+ */
+struct AbstractStaticEvaluator {
+  /** 
+   * @param b
+   * @param player
+   * @param depth
+   *
+   * @return 
+   */
+  virtual int operator()(const Board& b, Board::Player player, int depth) const = 0;
+};
+
+
+#endif
