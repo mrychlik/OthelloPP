5a97356be4ce07ae2ced20f6ed24657820e21ceb
Marek Rychlik <rychlik@gmail.com>
2021-05-26 11:41:57 -0700

    *** empty log message ***

--- MainLoop.hpp
+++ MainLoop.hpp
@@ -12,13 +12,15 @@
 #define MAIN_LOOP_H
 
 #include <iosfwd>
+#include "StaticEvaluator.hpp"
 
 extern int  max_depth[2];
 extern bool humanPlayer[2];
 extern int num_games;
 extern int computer_delay;
 
-extern int main_loop(std::istream& ins, std::ostream& os, std::ostream& logs);
+extern int main_loop(const StaticEvaluatorTable& evaluator, std::istream& ins, std::ostream& os, std::ostream& logs);
+extern int main_loop(const StaticEvaluatorTable& evaluator);
 extern int main_loop();
 
 #endif /* MAIN_LOOP */
