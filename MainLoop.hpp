91e9c5064984c54561a9647de4c7ca4e0a2168ea
Marek Rychlik <rychlik@gmail.com>
2021-05-26 14:34:17 -0700

    *** empty log message ***

--- MainLoop.hpp
+++ MainLoop.hpp
@@ -5,15 +5,26 @@
  * 
  * @brief  Declarations for main loop implementation
  * 
- * 
  */
 
 #ifndef MAIN_LOOP_H
 #define MAIN_LOOP_H
 
 #include <iosfwd>
+#include <memory>
 #include "StaticEvaluator.hpp"
 
+/**
+ * This class runs the game loop and controls 
+ * numerous game settings. It provides "fluid" style interface for ease of use,
+ * e.g.:
+ *
+ * MainLoop::getInstance()
+ *   ->setMaxDepth(Board::WHITE, 12)
+ *   ->setMaxDepth(Board::BLACK, 12)  
+ *   ->setNumGames(1)
+ *   ->run();
+*/
 class MainLoop {
 private:
 
@@ -22,19 +33,55 @@ private:
   static const int DEFAULT_COMPUTER_DELAY = 0; /**< Amount of delay in sec. after computer move */
 
 public:
+
+  static const MainLoop* getInstance() {
+    // The only instance this class will ever have
+    static MainLoop mainLoopSingleton;
+    return &mainLoopSingleton;
+  }
+  
+  auto setMaxDepth(Board::Player player, int depth) const {
+    max_depth[player] = depth;
+    return this;
+  }
+
+  auto setHumanPlayer(Board::Player player, bool isHuman) const {
+    humanPlayer[player] = isHuman;
+    return this;
+  }
+
+  auto setComputerDelay(int delay) const {
+    computer_delay = delay;
+    return this;
+  }
+
+  auto setNumGames(int numGames) const {
+    num_games = numGames;
+    return this;
+  }
+
+  auto setClearScreenBbeforePrinting(bool clear) const {
+    Board::clear_screen_before_printing = clear;
+  }
+
+  const MainLoop* reportSettings() const ;
+
+private:
+  MainLoop() = default;
+
   static int  max_depth[2]; ; /**< Max. depth for minmax play for each player*/
   static bool humanPlayer[2]; /**< Which player is human? */
   static int  num_games;      /**< Number of games to play */
   static int  computer_delay; /**< Number of seconds to wait after computer move */
-
   static const StaticEvaluatorTable& DEFAULT_EVALUATOR_TABLE;
 
-  static int main_loop(std::istream& ins,
-		     std::ostream& os,
-		     std::ostream& logs,
-		     const StaticEvaluatorTable& evaluatorTab = DEFAULT_EVALUATOR_TABLE);
+public:
+  static int run(std::istream& ins,
+		 std::ostream& os,
+		 std::ostream& logs,
+		 const StaticEvaluatorTable& evaluatorTab = DEFAULT_EVALUATOR_TABLE);
 
-  static int main_loop(const StaticEvaluatorTable& evaluatorTab = DEFAULT_EVALUATOR_TABLE);
+  static int run(const StaticEvaluatorTable& evaluatorTab = DEFAULT_EVALUATOR_TABLE);
 
 private:
 
